16d9581f9a1f073ff0a2dcaf50065659536e8221
anakazachkowa <anakazachkowa@yandex.ru>
2023-06-09 22:05:03 +0400

    hi

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,15 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+
         System.out.println(bean.getMessage());
+        System.out.println(cat.getMessage());
+        System.out.println("Первый вариант - " + (bean==bean1));
+        System.out.println("Второй вариант - " + (cat==cat1));
     }
 }
--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -10,5 +10,15 @@ public class AppConfig {
         HelloWorld helloWorld = new HelloWorld();
         helloWorld.setMessage("Hello World!");
         return helloWorld;
+
     }
+
+    @Bean(name="cat")
+    @Scope("prototype")
+    public Cat getCat() {
+        Cat cat = new Cat();
+        cat.setMessage("Cat");
+        return cat;
+    }
+
 }
